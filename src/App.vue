84a08ff18a01257b05fc73817f13907e60376784
Neo <her_liu@qq.com>
2022-03-14 20:34:11 +0800

    test finished

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import { NLoadingBarProvider } from 'naive-ui'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <n-loading-bar-provider class="app">
+    <HelloWorld />
+  </n-loading-bar-provider>
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 20px;
 }
 </style>
